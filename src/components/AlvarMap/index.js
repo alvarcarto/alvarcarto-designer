b50f7b799185847e62eead5614fbffcb0d2197f4
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2016-12-04 23:56:18 +0200

    Update to alvar tiles

--- src/components/AlvarMap/index.js
+++ src/components/AlvarMap/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Spin } from 'antd';
 import { Map as LeafletMap, Marker, Popup, TileLayer } from 'react-leaflet';
-//import ReactMapboxGl, { Layer, Feature, Marker } from "react-mapbox-gl";
 import './index.css';
 
 const FINLAND_CENTER = [63.744139, 24.669082];
@@ -11,8 +10,9 @@ const AlvarMap = React.createClass({
   getInitialState() {
     return {
       mapCenter: FINLAND_CENTER,
-      zoom: 4,
+      zoom: 10,
       loading: true,
+      theme: 'alvar',
     };
   },
 
@@ -21,7 +21,7 @@ const AlvarMap = React.createClass({
       () => this.setState(() => ({ loading: false })),
       200
     );
-    setTimeout(() => this._flyTo(HELSINKI_CENTER, 11.5), 1000);
+    setTimeout(() => this._flyTo(HELSINKI_CENTER, 12), 1000);
     //setTimeout(() => this._flyTo(HELSINKI_CENTER, 10), 1000);
   },
 
@@ -32,16 +32,6 @@ const AlvarMap = React.createClass({
       <div className="AlvarMap" style={{width: props.width, height: props.height}}>
         <div className="AlvarMap__container">
           <Spin spinning={state.loading}>
-            { /*
-            <ReactMapboxGl
-              center={state.mapCenter}
-              zoom={[state.zoom]}
-              onStyleLoad={this._onStyleLoad}
-              style="mapbox://styles/alvarcarto/ciwaqieka00622qpmqgjen1rd"
-              accessToken="pk.eyJ1IjoiYWx2YXJjYXJ0byIsImEiOiJjaXdhb2s5Y24wMDJ6Mm9vNjVvNXdqeDRvIn0.wC2GAwpt9ggrV-mGAD_E0w"
-            />
-            */
-            }
             <LeafletMap
               animate
               useFlyTo
@@ -50,18 +40,13 @@ const AlvarMap = React.createClass({
             >
               <TileLayer
                 detectRetina
-                url='http://tiles.alvarcarto.com:8080/{z}/{x}/{y}.png'
+                url={`http://tiles.alvarcarto.com:8080/${state.theme}/{z}/{x}/{y}/tile.png`}
               />
             </LeafletMap>
           </Spin>
         </div>
       </div>
     );
-  },
-
-  _onStyleLoad() {
-
-
   },
 
   _flyTo(pos, zoom) {
