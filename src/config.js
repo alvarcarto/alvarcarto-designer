1341d405c26a0ca8c3eb68a14a418be9aa391922
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-02-16 19:57:45 +0200

    Fix API_URL -> REACT_APP_API_URL

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ import _ from 'lodash';
 const CONST = {
   PUBLIC_URL: process.env.PUBLIC_URL,
   DEVELOPMENT: process.env.NODE_ENV === 'development',
-  API_URL: process.env.API_URL || 'http://localhost:9000',
+  REACT_APP_API_URL: process.env.REACT_APP_API_URL || 'http://localhost:9000',
 };
 
-if (_.endsWith(CONST.API_URL, '/')) {
-  throw new Error('Trailing slash in API_URL. Remove it.');
+if (_.endsWith(CONST.REACT_APP_API_URL, '/')) {
+  throw new Error('Trailing slash in REACT_APP_API_URL. Remove it.');
 }
 
 export default CONST;
