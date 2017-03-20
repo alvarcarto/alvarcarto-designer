a6a36c24773deed8d0092ec9ac108d98b21e7a7e
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-03-20 12:16:50 +0200

    Make google font loading async

--- src/init-libs.js
+++ src/init-libs.js
@@ -1,11 +1,15 @@
 /* eslint-disable */
 
-// Install Sentry client
 import Raven from 'raven-js';
+import WebFont from 'webfontloader';
+
+// Install Sentry client
 Raven.config('https://2456dddf26df432bae36dd00ea1d2a45@sentry.io/148773').install();
 
+// Stripe
 Stripe.setPublishableKey('pk_test_PKlD56JoqROJNxZa8JoV5ILr');
 
+// Hotjar
 (function(h,o,t,j,a,r){
   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
   h._hjSettings={hjid:437392,hjsv:5};
@@ -15,6 +19,18 @@ Stripe.setPublishableKey('pk_test_PKlD56JoqROJNxZa8JoV5ILr');
   a.appendChild(r);
 })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
 
+// Google fonts
+WebFont.load({
+  google: {
+    families: [
+      'Fira Sans Extra Condensed:400,700',
+      'Source Sans Pro:200,400,700',
+      'Roboto Condensed:300,400,700',
+    ],
+  },
+});
+
+// Google Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
