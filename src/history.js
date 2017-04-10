2bd136d04b5315247584209f0bbb1f12d99528d8
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-04-10 22:07:23 +0300

    Add google analytics events on history change

--- src/history.js
+++ src/history.js
@@ -1,3 +1,10 @@
+/* global ga */
+
 import { createBrowserHistory } from 'history';
 const history = createBrowserHistory();
+history.listen((location) => {
+  window.ga('set', 'page', location.pathname + location.search);
+  window.ga('send', 'pageview');
+});
+
 export default history;
