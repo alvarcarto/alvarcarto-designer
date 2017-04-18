5eb64ccdd8b8b87856938413dfe7361e10ab43d2
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-04-18 03:30:34 +0300

    Fix a few responsive bugs

--- src/constants.js
+++ src/constants.js
@@ -2,10 +2,12 @@ const CONST = {
   SYMBOL_EUR: '\u20AC',
   PRIMARY_COLOR: '#0084FF',
   EMPTY_MAP_PADDING_FACTOR: 0.03,
-  SCREEN_XS: 480,
-  SCREEN_SM: 768,
-  SCREEN_MD: 992,
-  SCREEN_LG: 120,
+  // We are using maxWidth in all MediaQuery components, let's
+  // -1 px to be consistent with CSS ranges
+  SCREEN_XS: 480 - 1,
+  SCREEN_SM: 768 - 1,
+  SCREEN_MD: 992 - 1,
+  SCREEN_LG: 120 - 1,
 };
 
 export default CONST;
